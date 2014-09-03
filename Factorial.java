641e6d633cc951ba314a215339b2f18e44392e47
Richard <richard.wasniowski@csuci.edu>
2014-09-03 05:16:30 -0700

    My second commit

--- Factorial.java
+++ Factorial.java
@@ -20,6 +20,7 @@ public class Factorial {
             fact = fact*c;
  
          System.out.println("Factorial of "+n+" is = "+fact);
+         System.out.println("Changes after My first commit!");
       }
 
     
